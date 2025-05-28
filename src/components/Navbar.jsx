1a38123b93a9f4d25b946c854519b40abd53e890
heyharsh50 <harshraj88253@gmail.com>
2025-05-28 21:25:04 +0530

    New logo pic and fix the name issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import pic from "../../public/photo.avif";
+import pic from "../../public/photo1.jpg";
 import { AiOutlineMenu } from "react-icons/ai";
 import { IoCloseSharp } from "react-icons/io5";
 import { Link } from "react-scroll";
@@ -32,9 +32,9 @@ function Navbar() {
       <div className="max-w-screen-2xl container mx-auto px-4 md:px-20 h-16 shadow-md fixed top-0 left-0 right-0 z-50 bg-white">
         <div className="flex justify-between items-center h-16">
           <div className=" flex space-x-2">
-            <img src={pic} className="h-12 w-12 rounded-full" alt="" />
+            <img src={pic} className="h-12 w-12 rounded-full object-cover border-2 border-white shadow-sm"alt="" />
             <h1 className="font-semibold text-xl cursor-pointer">
-              Hars<span className="text-green-500 text-2xl">h</span>
+              <span className="text-black-500 text-2xl">H</span>arsh Raj
               <p className="text-sm">Web Developer</p>
             </h1>
           </div>
